38f8d4bb14bb28688511708c8cb3e19e9cfb7a35
rohyeeet <66509575+rohyeeet@users.noreply.github.com>
2020-10-20 20:52:16 +0530

    Update emailphonescraping.py

--- emailphonescraping.py
+++ emailphonescraping.py
@@ -12,21 +12,7 @@ import re
 from urllib.request import urlopen
 
 f = urlopen('http://www.bc.edu/a-z/directories/contact/quicknos.html')
-
-s = BeautifulSoup(f, 'html.parser')
-s = s.get_text()
-
-phone = re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})",s)
-emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}",s)
-
-if len(phone) == 0:
-    print ("Sorry, no phone number found.")
-
-    print('------------')
-    print ()
-else :
-    count = 1
-    for item in phone:
+\STUPID IDIOT
         print ( count, ' phone number(s) found : ',item )
         count += 1
 
